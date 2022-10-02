a8667be278d91880ec3d419dce73b50e05c05c6b
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-02 19:33:52 +0100

    add home components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Home from './components/Home';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import './home'
+import React from 'react';
+import Image from '../img/image 1.png';
+import './home.css';
 
-const Home = () => {
-  return (
-    <div className='home-container'>
-      <div className='home-wrapper'>
-        
+const Home = () => (
+  <section className="home-container">
+    <div className="home-wrapper">
+      <div className="img-container">
+        <img src={Image} alt="A movie night" />
+      </div>
+      <div className="info-container">
+        <h2>Imagine if Snapchat had events.</h2>
+        <p>
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+        <button type="button" className="btn btn-primary">
+          Create my event
+        </button>
       </div>
     </div>
-  )
-}
+  </section>
+);
 
-export default Home
+export default Home;
