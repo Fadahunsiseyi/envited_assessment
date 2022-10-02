a7a95a264f67280d455e78f7a1c4ba9aab1416a5
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-02 22:23:11 +0100

    add create page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import EventDetails from './pages/EventDetails';
+import Event from './pages/Event';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/create" element={<EventDetails />} />
+          <Route path="/event" element={<Event />} />
         </Routes>
       </BrowserRouter>
     </div>
