6de4611f2e8321dbead37a73f8bc62e9899bd05d
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-03 11:31:51 +0100

    fix navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 import Home from './components/Home';
 import EventDetails from './pages/EventDetails';
 import Event from './pages/Event';
 
 function App() {
+  const [inputVals, setInputVals] = useState({});
+  const onSubmitForm = (input) => {
+    setInputVals(input);
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/create" element={<EventDetails />} />
-          <Route path="/event" element={<Event />} />
+          <Route path="/create" element={<EventDetails onSubmitForm={onSubmitForm} />} />
+          <Route path="/event" element={<Event inputs={inputVals} />} />
         </Routes>
       </BrowserRouter>
     </div>
